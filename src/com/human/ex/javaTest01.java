3f167b36595ee5a2e71bce966243051e0da26207
admin <admin@DESKTOP-HNPELAV>
2022-03-03 14:26:18 +0900

    first commit

---
+++ src/com/human/ex/javaTest01.java
@@ -0,0 +1,43 @@
+package com.human.ex;
+
+public class javaTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        if(9<1) {
+        	System.out.println("true");
+        	System.out.println("yasyayay~");
+        }else {
+        	System.out.println("yacorging");
+        }
+        int a=1;
+        switch(a){
+        	case 1:
+        		System.out.println("korea");
+        	break;
+        	case 2:
+        	    System.out.println("america");
+        	break;
+        	case 3:
+        		System.out.println("rusia");
+        	break;
+        	default:
+        		System.out.println("china");
+        	
+        		
+        }
+        int j=21;
+       		
+        if(j<=20) {
+        	System.out.println("학생입니다");
+        }else {
+        	System.out.println("나쁜으른이~");
+        }
+        
+		System.out.println("나는야최강호랑이");
+		System.out.println("아타호는 린샹이랑 이어지는가");
+	    System.out.println("바바ㅏ바밤 바바바아바아바ㅏ바밥~~!");
+	    System.out.println("we are the chapion~ my friend~");
+	}
+
+}
